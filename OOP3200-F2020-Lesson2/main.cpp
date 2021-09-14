1ec1ca33782fdb98da097f8c7f7085f95dedfc1e
Nicholas Shortt <Nicholas.Shortt@hotmail.com>
2021-09-14 19:56:45 -0400

    Initial commit

--- OOP3200-F2020-Lesson2/main.cpp
+++ OOP3200-F2020-Lesson2/main.cpp
@@ -111,20 +111,20 @@ int main()
 *	@return	the distance as a double
 */
 
-{
-	// difference between x values
-	// difference between y values
-	// return the formula (based on Pythagorean theorem)
-
-}
+//{
+//	// difference between x values
+//	// difference between y values
+//	// return the formula (based on Pythagorean theorem)
+//
+//}
 
 /** ToString Method for CartesianPoint class
 *	Converts the obj to a string.
 *	@return	the obj state as a string
 */
 
-{
-	// declare a stringstream object
-	// build the string
-	// return the string
-}
+//{
+//	// declare a stringstream object
+//	// build the string
+//	// return the string
+//}
